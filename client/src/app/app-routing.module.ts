051f35c6b01bb067e78a0b2caeb93f6fdd800487
jessica <jessicahenry2604@gmail.com>
2022-10-15 15:42:17 +0100

    add user routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'users/:id', component:UserProfileComponent
+  },
+  {
+    path:'home', component:AppComponent
+  }
+ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
