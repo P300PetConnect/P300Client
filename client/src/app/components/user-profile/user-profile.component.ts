f9ff2aa1a3ae3e5bdc5109e1cdcfb544971e8d7d
jessica <jessicahenry2604@gmail.com>
2022-10-18 00:51:45 +0100

    create some local data & screen idea - PSitter

--- client/src/app/components/user-profile/user-profile.component.ts
+++ client/src/app/components/user-profile/user-profile.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
+  skills = ["Pet Walker", "Pet Minder", "Overnight", "Weekly", "Monthly"]   //initialization
+  petsitter = 
+    { id:"1",
+      name:'Anne Smith', 
+      title:"Dog care", 
+      location:"Sligo Town",
+      description:"Hi we are looking for a dog walker to walk our lovely dog Frankie during the day and to keep her company for a few hours while we are at work. Require..."
+    } ; //initialization
+
   
   constructor() { }
 
