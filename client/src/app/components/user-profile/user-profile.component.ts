125cd28edc50b8a55a929dfbd227e839de4a572f
jessica <jessicahenry2604@gmail.com>
2022-10-18 13:38:56 +0100

    working with local data

--- client/src/app/components/user-profile/user-profile.component.ts
+++ client/src/app/components/user-profile/user-profile.component.ts
@@ -7,20 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  skills = ["Pet Walker", "Pet Minder", "Overnight", "Weekly", "Monthly"]   //initialization
+   //initialization
   petsitter = 
     { id:"1",
       name:'Anne Smith', 
       title:"Dog care", 
       location:"Sligo Town",
-      description:"Hi we are looking for a dog walker to walk our lovely dog Frankie during the day and to keep her company for a few hours while we are at work. Require...",
+      description:"Personalised and flexible pet care,I provide one-on-one love and attention in a cosy home space.",
       imageUrl:"https://demos.creative-tim.com/argon-dashboard/assets-old/img/theme/team-4.jpg", 
       Location:"Sligo, Town", 
-      CreatedDate:"14/10/2022"
+      CreatedDate:"14/10/2022", 
+      skills:["Pet Walker", "Pet Minder", "Overnight", "Weekly", "Monthly"], 
+      pettype:["Dog", "Cat", "Fish"]
     
-    } ; //initialization
-
-  
+    } ; 
   constructor() { }
 
   ngOnInit(): void {
