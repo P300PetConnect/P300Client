05054365a5976dce97321779530f90cfc4d3a52d
StephenMeehan1996 <78025435+StephenMeehan1996@users.noreply.github.com>
2022-10-25 09:26:11 +0100

    working post example

--- client/src/app/forum-services/data.service.ts
+++ client/src/app/forum-services/data.service.ts
@@ -1,5 +1,5 @@
 import { JsonPipe } from '@angular/common';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry,tap,map } from 'rxjs/operators';
@@ -22,14 +22,38 @@ export class DataService {
   
     );
 }
-
-    PushPost(item : PostItem)
+/**
+{
+  "PostID": "345345555555",
+  "PostTitle": "Please wordsdds",
+  "Content": "asdasdasdasd",
+  "Date": "213123123",
+  "DisplayComments": false,
+  "VoteCount": 0
+}
+ */
+    postId;
+    PushPost(Item : PostItem)
     {
-     console.log(JSON.stringify(item));
-      this.http.post('https://4pms4upawl.execute-api.eu-west-1.amazonaws.com/new/post', JSON.stringify(item))
-      .subscribe((res) =>{
-        console.log(res);
-      });
+      const headers_j = new HttpHeaders({ 'Content-Type': 'application/json' });
+      const headers_H = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
+      const bodyx = 'PostID=12334234&PostTitle=PleaseWork&Content=testing&Date=213121&DisplayComments=false&VoteCount=0'
+      const bodyj = (JSON.stringify(Item));
+      console.log(JSON.stringify(Item))
+
+     const b: Array<any> = [
+        { PostID: "book1", PostTitle: "book desc 1" }
+        
+      ];
+      // this.http.post<any>('https://4pms4upawl.execute-api.eu-west-1.amazonaws.com/new/post', b,{headers: headers_j})
+      // .subscribe((res) =>{
+      //   console.log(res);
+      // });
+
+      //working
+      this.http.post<any>('https://4pms4upawl.execute-api.eu-west-1.amazonaws.com/new/post', { PostID: '23123123', PostTitle : 'Testing', Content: 'Is this working', Date: "25/10/22", DisplayComments: false, VoteCount: 0}).subscribe(data => {
+        this.postId = data.id;
+    })
 
     }
 }
