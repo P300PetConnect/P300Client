7dc3531654f2e054f377996ca2a5e63d58be7439
jessica <jessicahenry2604@gmail.com>
2022-10-31 19:11:19 +0000

    complete profile - getting user name

--- client/src/app/components/userform/userform.component.ts
+++ client/src/app/components/userform/userform.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UntypedFormGroup } from '@angular/forms';
-
+import { IUser } from '../interfaces/users';
+import { UserService } from '../service/data.service';
 @Component({
   selector: 'app-userform',
   templateUrl: './userform.component.html',
@@ -8,10 +9,25 @@ import { UntypedFormGroup } from '@angular/forms';
 })
 export class UserformComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _serviceGetUser: UserService) { }
+
+userData?: IUser; 
 form: UntypedFormGroup; 
 private formSubmitAttemp:boolean; 
 
-  ngOnInit(): void { }
+  ngOnInit(): void { 
+    this.getUser(); 
+    console.log(this?.userData?.name)
+  }
 
-}
+
+  getUser(){
+    this._serviceGetUser.get_user("Frank@hotmail.com").subscribe(
+      userData=>{
+        this.userData = userData;
+        console.log(userData)
+      }); 
+      return false; 
+    }
+  }
+
