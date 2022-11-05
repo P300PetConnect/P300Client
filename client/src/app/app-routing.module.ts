860d5b4c869618ed7196a755144b95875f6a5ae1
jessica <jessicahenry2604@gmail.com>
2022-11-05 14:36:05 +0000

    routing to profile dropdown

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,17 +6,18 @@ import { InitialPageComponent } from './components/initial-page/initial-page.com
 import { LoginComponent } from './components/login/login.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { UserformComponent } from './components/userform/userform.component';
-
+import {SearchPetServiceComponent} from './components/search-pet-service/search-pet-service.component';
  authenticator: AuthenticatorService
 
 const routes: Routes = [
-  {path:'user', component:UserProfileComponent},
-  {path:'', pathMatch: 'full', redirectTo:'initial'},
+  {path:'user-profile', component:UserProfileComponent},
+  {path:'', pathMatch: 'full', redirectTo:'search'},
   { path:'initial', component:InitialPageComponent},
   { path:'initial/login/petowner', component:LoginComponent},
   { path:'initial/login/petminder', component:LoginComponent},
   { path:'user-form', component:UserformComponent, },
   { path:'login', component:LoginComponent }, 
+  {path:'search', component:SearchPetServiceComponent}
  ];
 
 @NgModule({
