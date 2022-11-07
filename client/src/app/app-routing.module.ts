0765e77ae3ba8bf53f10ceceeee6c62de705e1c4
StephenMeehan1996 <78025435+StephenMeehan1996@users.noreply.github.com>
2022-11-07 13:35:27 +0000

    merge

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { CognitoGuard } from './cognito.guard';
 import { InitialPageComponent } from './components/initial-page/initial-page.component';
 import { LoginComponent } from './components/login/login.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { ForumWallComponent } from './forum-components/forum-wall/forum-wall.component';
 
 const routes: Routes = [
   {
     path:'users/:id', component:UserProfileComponent
   },
   {
-    path:'home', component:AppComponent
+    //path:'home', component:AppComponent
+    path:'forum', component:ForumWallComponent
   }
  ];
 
