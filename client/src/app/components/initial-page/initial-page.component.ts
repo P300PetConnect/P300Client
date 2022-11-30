a8872fc5d85eff6960616fd65841f9c0bd5acb5b
jessica <jessicahenry2604@gmail.com>
2022-11-30 21:23:21 +0000

    home page

--- client/src/app/components/initial-page/initial-page.component.ts
+++ client/src/app/components/initial-page/initial-page.component.ts
@@ -1,8 +1,7 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
-import { IUser } from '../interfaces/form';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { UserService } from '../service/user.service';
-// import Swal from 'sweetalert2/dist/sweetalert2.js';
-import { DialogComponent } from '../dialog/dialog.component';
+import { FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-initial-page',
@@ -13,16 +12,38 @@ export class InitialPageComponent implements OnInit {
 formFields: any;
 @Input() userEmail?: any; 
 user?: any; 
+petsize = new FormControl('');
+petsizeList: string[] = ['up to 5 kg', '5-10 kg', '10-20 kg', '20-40 kg', '+40kg'];
+
+@ViewChild('sleepover') sleepover: ElementRef;
+@ViewChild('creche') creche: ElementRef;
+
+
+images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
   constructor(private _userService: UserService) { }
 
   displayAlert: boolean = false; 
 
   ngOnInit(): void {
-    // this.userData()
-  
   }
 
+  setSelected(id:number){
+    console.log(id); 
+    if(id==1){
+      this.sleepover.nativeElement.hidden = false; 
+      this.creche.nativeElement.hidden = true; 
+    }
+    else if(id==2){
+      this.sleepover.nativeElement.hidden = true; 
+      this.creche.nativeElement.show = true; 
+  }
+  }
+
+
+  SearchService(){
+    this.sleepover.nativeElement.hidden; 
+  }
   getStarted(){
     if(this.displayAlert){
       this.displayAlert = false;
@@ -31,17 +52,5 @@ user?: any;
       this.displayAlert = true; 
     }
   }
-  // userData():boolean{
-  //     this?._userService.get_user(this?.userEmail).subscribe(
-  //       user=>{
-  //         this.user=user; 
-  //         console.log('we did it bitch',this?.user); 
-  //       },
-  //     // error => this.errorMessage = <any>error
-  //     );
-  //   return false; 
-  //   }
 
-
-  
 }
