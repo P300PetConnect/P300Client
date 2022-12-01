dac890ad8a93cfed0ad9abd99d6c58627417b403
jessica <jessicahenry2604@gmail.com>
2022-12-01 01:16:36 +0000

    input home page

--- client/src/app/components/initial-page/initial-page.component.ts
+++ client/src/app/components/initial-page/initial-page.component.ts
@@ -12,6 +12,11 @@ export class InitialPageComponent implements OnInit {
 formFields: any;
 @Input() userEmail?: any; 
 user?: any; 
+isActive: boolean = true; 
+secondIsActive: boolean = false; 
+thirdIsActive: boolean = false; 
+fourthIsActive: boolean = false; 
+
 petsize = new FormControl('');
 petsizeList: string[] = ['up to 5 kg', '5-10 kg', '10-20 kg', '20-40 kg', '+40kg'];
 
@@ -24,7 +29,9 @@ images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
   constructor(private _userService: UserService) { }
 
   displayAlert: boolean = false; 
+  selectedIndex: number = null;
 
+  
   ngOnInit(): void {
   }
 
@@ -32,12 +39,31 @@ images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
     console.log(id); 
     if(id==1){
       this.sleepover.nativeElement.hidden = false; 
-      this.creche.nativeElement.hidden = true; 
+      this.isActive = true;
+      this.fourthIsActive = false;
+      this.secondIsActive=false; 
+      this.thirdIsActive=false;  
     }
     else if(id==2){
       this.sleepover.nativeElement.hidden = true; 
-      this.creche.nativeElement.show = true; 
-  }
+      this.isActive = false;
+      this.secondIsActive=true; 
+      this.thirdIsActive=false; 
+      this.fourthIsActive = false; 
+}
+else if(id==3){
+  this.sleepover.nativeElement.hidden = true; 
+  this.isActive = false;
+  this.secondIsActive=false;
+  this.thirdIsActive=true; 
+}
+else if(id==4){
+  this.sleepover.nativeElement.hidden = true; 
+  this.isActive = false;
+  this.secondIsActive=false;
+  this.thirdIsActive=false; 
+  this.fourthIsActive = true; 
+}
   }
 
 
