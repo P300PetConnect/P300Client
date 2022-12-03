daf9d1b8ad58731094e9eb9033e20e343394775a
jessica <jessicahenry2604@gmail.com>
2022-12-03 18:58:05 +0000

    change the routing name

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
   { path:'initial', component:InitialPageComponent }, 
   { path:'', component:InitialPageComponent }, 
   { path:'login', component:LoginComponent },
-  { path:'petsitter', component:PetSitterDetailsComponent } 
+  { path:'petsitterdetails', component:PetSitterDetailsComponent } 
 ];
 
 @NgModule({
