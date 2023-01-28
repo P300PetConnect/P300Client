cb2d9e96f17e03679694cf09734fa5a7c57f50a4
S00213963 <stephenmeehan1996@gmail.com>
2023-01-28 12:58:08 +0000

    fix after mege

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -82,7 +82,7 @@ import { ChunkPipe } from './calender-pipe/chunk.pipe';
 import { SearchVersion2Component } from './components/search-version2/search-version2.component';
 import { PetSitterDetailsComponent } from './components/pet-sitter-details/pet-sitter-details.component';
 import { SearchSitterServicesComponent } from './components/search-sitter-services/search-sitter-services.component';
-import { ReviewFormComponent } from './ReviewComponents/review-form/review-form.component';
+
 import { OrderComponent } from './components/order/order.component';
 import { ReviewFormComponent } from './ReviewComponents/review-form/review-form.component';
 import { ReviewComponent } from './ReviewComponents/review/review.component';
