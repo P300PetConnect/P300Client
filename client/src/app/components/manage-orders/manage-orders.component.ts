5e7c074ffeea78d14e5a854e482ba5a90716a669
jessica <jessicahenry2604@gmail.com>
2023-01-30 21:35:56 +0000

    order management done, TODO: UI improv.

--- client/src/app/components/manage-orders/manage-orders.component.ts
+++ client/src/app/components/manage-orders/manage-orders.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Directive, EventEmitter, OnInit, Output } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { AuthenticatorService } from '@aws-amplify/ui-angular';
-import { IOrder } from '../interfaces/form';
+import { IOrder, IUser } from '../interfaces/form';
 import { IPetOwner } from '../interfaces/users';
+import { OrderComponent } from '../order/order.component';
 import { OrderService } from '../service/order.service';
 import { UserService } from '../service/user.service';
 
@@ -12,31 +14,101 @@ import { UserService } from '../service/user.service';
 })
 export class ManageOrdersComponent implements OnInit {
   
+  @Output() onCreate: EventEmitter<any> = new EventEmitter<any>();
   panelOpenState = false;
   panelOpenState2 = false; 
   orders:IOrder[]=[]; 
 
   ordesTest:any; 
   i: number; 
-  client: IPetOwner; 
+  clients: any = {}; 
+  petOwners: any;
+  errorMessage: any;
+  petSitter: import("/Users/jessicahenry/Project300Backup 2/client/src/app/components/interfaces/users").IPetSitter;
   
-  constructor(private _httpOrder:OrderService, public authenticator: AuthenticatorService, private _httpUser: UserService) {
+  constructor(private _httpOrder:OrderService,private dialog:MatDialog, public authenticator: AuthenticatorService, private _httpUser: UserService) {
+   }
+   ngOnInit():void{
+    this.getOrders(); 
    }
 
-  async ngOnInit(): Promise<void> {
-   const data = await this.getOrders(); 
-}
 
-  GetClientDetails(email:string){
-    this._httpUser.get_petowner(email).subscribe(client => this.client = client);
-    console.log('Client',JSON.stringify((this.client)))
+  //  async getOrders(){
+  //   const orders = await this._httpOrder.getOderByUser(this.authenticator?.user?.attributes?.email).toPromise()
+  //   this.orders = orders;
+  //   this.getClients(this.orders[3]?.PetOwnerID); 
+  //   this.orders.forEach(element => {
+  //     this.getClients(element?.PetOwnerID).then(petOwner => this.clients.push(petOwner));
+  //   });
+  // }
+
+  //  async getClients(petOwnerId){
+  //   const customers = await this._httpUser.get_petownerByUserID(petOwnerId).toPromise(); 
+  //   this.petOwners=customers; 
+  //   return customers; 
+  //  }
+
+  // async getOrders(){
+  //   const orders = await this._httpOrder.getOderByUser(this.authenticator?.user?.attributes?.email).toPromise()
+  //   this.orders = orders;
+  //   this.clients = [];
+  //   this.orders.forEach(element => {
+  //     this.getClients(element?.PetOwnerID).then(petOwner => this.clients.push(petOwner));
+  //   });
+  // }
+  
+  // async getClients(petOwnerId){
+  //   const customers = await this._httpUser.get_petownerByUserID(petOwnerId).toPromise(); 
+  //   this.petOwners=customers; 
+  //   return customers; 
+  // }
+  
+  // async getOrders() {
+  //   const orders = await this._httpOrder.getOderByUser(this.authenticator?.user?.attributes?.email).toPromise()
+  //   this.orders = orders;
+  //   this.clients = [];
+  
+  //   const promises = this.orders.map(element => this.getClients(element?.PetOwnerID));
+  //   this.clients = await Promise.all(promises);
+  
+  //   console.log('Orders', this.orders);
+  //   console.log('Clients:', this.clients);
+  // }
+
+  // async getPetsitter(){
+  //   const petSitterInformation = await this._httpUser.get_petsitter().toPromise(); 
+  //   return petSitterInformation; 
+  // }
+
+  getPetSitter(){
+    this._httpUser.get_petsitter(this.authenticator?.user?.attributes?.email).subscribe(
+      async petSitter=>{
+        this.petSitter = petSitter;
+        console.log(petSitter)
+        const orders = await this._httpOrder.getOderByUser(  this.petSitter?.petSitterId).toPromise()
+        this.orders = orders;
+      }); 
+      return false; 
+    }
+
+
+
+  async getOrders() {
+    console.log(this.petSitter?.petSitterId); 
+    const orders = await this._httpOrder.getOderByUser(36).toPromise()
+    this.orders = orders;
+    console.log('Orders', this.orders);
   }
 
-  getOrders(){
-    this._httpOrder.getOderByUser(this.authenticator?.user?.attributes?.email).subscribe(orders => this.orders = orders);
-    console.log('Orders',JSON.stringify((this.orders)))
-
-   }
+  onCreateOrder(){
+    // this._userService.initializeFormGroup(); 
+    const dialogConfig = new MatDialogConfig(); 
+    dialogConfig.disableClose = false; 
+    dialogConfig.autoFocus = true; 
+    dialogConfig.width = "60%";
+    this.dialog.open(OrderComponent, dialogConfig); 
+  
+  }
 
 
 }
