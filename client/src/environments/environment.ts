811568b21aff59d994213b4d79713719cbc37ab2
jessica <jessicahenry2604@gmail.com>
2023-02-09 00:48:38 +0000

    prod

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -9,15 +9,19 @@
 // };
 
 export const environment = {
-  production: false,
-  stream: {
-    key: 'a584yba4xs7f'
-  },
   GOOGLE_MAPS_API_KEY: 'AIzaSyCz-Nu0ku-0DJEe5iPt13RTq0QVpiz45AY',
-  region: 'eu-central-1',
+  // production: true,
+  region: 'eu-west-1',
   identityPoolId: 'eu-west-1:632c2f15-6b41-44b0-9d26-539386e90e4d',
   userPoolId: 'eu-west-1_dkqFlijuX',
   clientId: '1kvja59f1tthl9chrrjn59pgou',
+  production: false,
+  stream: { key: 'a584yba4xs7f'},
+  // GOOGLE_MAPS_API_KEY: 'AIzaSyCz-Nu0ku-0DJEe5iPt13RTq0QVpiz45AY',
+  // region: 'eu-central-1',
+  // identityPoolId: 'eu-west-1:632c2f15-6b41-44b0-9d26-539386e90e4d',
+  // userPoolId: 'eu-west-1_dkqFlijuX',
+  // clientId: '1kvja59f1tthl9chrrjn59pgou',
   domainName: 'ui-61hb39v3652llcugcb7489b7e1',
   redirectDomain: 'http://localhost:4200',
   sts_endpoint: '',
