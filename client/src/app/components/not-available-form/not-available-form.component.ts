b18afb6e83bb7e36cbad48cf46b3a22b8d1132a8
S00213963 <stephenmeehan1996@gmail.com>
2023-02-12 14:53:37 +0000

    posts/deletes/gets working

--- client/src/app/components/not-available-form/not-available-form.component.ts
+++ client/src/app/components/not-available-form/not-available-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { INotAvailable, IOrderList } from '../interfaces/order';
 import { DatePipe } from '@angular/common';
 import { OrderService } from '../service/order.service';
@@ -12,10 +12,17 @@ import { OrderService } from '../service/order.service';
 export class NotAvailableFormComponent implements OnInit {
 
   /*
+  -change so selected orders for the day can be more than 1
+  -style the not available form
+  -move calander to profile view will all events x't out
+ 
   
   */
   @Input() orders: IOrderList [] = [];
+  @Input() userID: number;
   @Input() notAvailble: INotAvailable [] = [];
+  @Output() newItemEvent = new EventEmitter();
+  @Output() closeForm = new EventEmitter();
   message: any;
   
   constructor(private datePipe: DatePipe, private _order: OrderService) { }
@@ -27,10 +34,11 @@ export class NotAvailableFormComponent implements OnInit {
   {
    
     date = this.datePipe.transform(new Date(date), 'dd MMM yyyy');
+    alert(this.userID);
 
     let obj = 
     {
-      "UserID": 43,
+      "UserID": this.userID,
       "TimeStamp" : date
 
     }
@@ -38,12 +46,33 @@ export class NotAvailableFormComponent implements OnInit {
     this._order.addNotAvailable(obj).subscribe({
       next: nAvailable => {
         console.log(JSON.stringify(nAvailable) + 'nAvailable added');
-        this.message = "list added";
-        
+        this.message = "nAvailable added";
+        this.newItemEvent.emit();
        
          },
       error: (err) => this.message = err
     });
+  }
+  DeleteNotAvailable(id: number)
+  {
+    
+    this._order.DeleteItem(id).subscribe({
+      next: nAvailable => {
+        console.log(JSON.stringify(nAvailable) + 'nAvailable removed');
+        this.message = "movie added";
+        this.newItemEvent.emit();    
+         },
+      error: (err) => this.message = err
+    });
+  }
+
+  refresh()
+  {
+    this.newItemEvent.emit();
+  }
+  CloseForm()
+  {
+    this.closeForm.emit();
   }
 
 }
