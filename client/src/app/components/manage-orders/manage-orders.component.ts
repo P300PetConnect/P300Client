70f14e540b7918864dd6fc2f7543790c6970fad3
jessica <jessicahenry2604@gmail.com>
2023-03-06 22:28:55 +0000

    fixing - see petsitter from order to details page

--- client/src/app/components/manage-orders/manage-orders.component.ts
+++ client/src/app/components/manage-orders/manage-orders.component.ts
@@ -45,7 +45,6 @@ export class ManageOrdersComponent implements OnInit {
 
     this.getOrders(); 
 
-  this.email = 'joannasmith@gmail.com'; 
    }
 
    async getOrders(){
@@ -54,7 +53,8 @@ export class ManageOrdersComponent implements OnInit {
       // this._httpUser.get_petsitter(this.authenticator?.user?.attributes?.email).subscribe(
         // async petSitter=>{
           // this.petSitter = petSitter;
-          const orders = await this._httpOrder.getOderByUser(36).toPromise()
+          console.log(this.petSitter?.petSitterId); 
+          const orders = await this._httpOrder.getOderByUser(this.petSitter?.petSitterId).toPromise()
           this.orders = orders;
           console.log('test', this.orders); 
           console.log(this.petSitter)
@@ -101,14 +101,14 @@ export class ManageOrdersComponent implements OnInit {
   
   }
 
-  GetPetOwnerDetails(email:string){
+  GetPetOwnerDetails(userID:number,email:string){
     
     console.log('call pet owner')
     if(this.userGroup =="PetSitter"){
       this._router.navigate(['/petownerprofile', {'id': `${email}`}])
     }
     else if(this.userGroup =="PetOwner"){
-        this._router.navigate(['/petsitterdetails', {'id': `${43}`}])
+        this._router.navigate(['/petsitterdetails', {'id': `${userID}`}])
     }
   }
 }
